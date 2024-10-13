ccafbf67471e821669c6288129212848dc6839f1
rye <58236573+ryejpeg@users.noreply.github.com>
2024-10-13 16:12:11 -0700

    Update taskGiver.cs
    Adds verification of task existence prior to addition, as to prevent task duplication in player's active task list. Task display update is no longer handled by taskGiver.cs.

--- scripts/taskGiver.cs
+++ scripts/taskGiver.cs
@@ -31,10 +31,30 @@ public class taskGiver : MonoBehaviour
 
     }
 
+    // Adds task to active tasks list once player clicks
     void OnMouseDown()
     {
-        var i = taskManag.makeTask(itaskName, itaskObjective, itaskPriority, true, false, itaskType, itaskID);
+        var IDexists = false;
 
-        taskDisplay.updateTaskDisplay();
+        // Cycles through all active tasks to check if the task is already added
+        for(int i = 0; i <= (taskManag.tasksActive.Count - 1); i++)
+        {
+            Debug.Log("taskGiver.cs: OnMouseDown(): return value of checkActiveIDexists() = " + taskManag.checkActiveIDexists(itaskID, i));
+
+            // Determines whether the task already exists at given index (identified by matching task ID)
+            if(taskManag.checkActiveIDexists(itaskID, i))
+            {
+                IDexists = true;
+                Debug.Log("taskGiver.cs: OnMouseDown(): ID match found -- IDexists = " + IDexists);
+            }
+        }
+
+        Debug.Log("taskGiver.cs: OnMouseDown(): after check -- IDexists = "+ IDexists);
+
+        // Adds task to active tasks list and updates display if it no matching ID is found
+        if(IDexists == false)
+        {
+            var i = taskManag.makeTask(itaskName, itaskObjective, itaskPriority, true, false, itaskType, itaskID);
+        }   
     }
 }
